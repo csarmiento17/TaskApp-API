7d82001c3784a5a1c87e038b976b64006e48b1e1
csarmiento17 <sarmientocarlopher@gmail.com>
2021-01-19 04:11:38 -0500

    Added Auto cropping and image formatting

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
@@ -132,8 +133,9 @@ const upload = multer({
     }
 })
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
     
-    req.user.avatar = req.file.buffer 
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
